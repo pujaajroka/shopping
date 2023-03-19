6d534a17dbcb8f684bd1326bca8abe5c434710e0
puja roka <pujaajroka@gmail.com>
2023-03-19 10:46:01 +0530

    admin  dashbord onwork

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,5 +1,5 @@
 
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers} from "@reduxjs/toolkit";
 
 import cartReducer from "./cartRedux";
 import userReducer from "./userRedux";
@@ -19,14 +19,11 @@ import {
     version: 1,
     storage,
   }
-  
-  const persistedReducer = persistReducer(persistConfig, userReducer)
+  const rootReducer = combineReducers({user:userReducer, cart:cartReducer })
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer:{
-        cart: cartReducer,
-        user: persistedReducer,
-    },
+    reducer:persistedReducer,
     middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
