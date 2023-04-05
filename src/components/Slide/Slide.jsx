a89964c0563427613884a8379cf31f807ce882f8
DESKTOP-72D0N0M\Dell <webfusionsolution@gmail.com>
2023-04-05 10:00:17 +0530

    xtyles client changes

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -13,8 +13,11 @@ function Slide(props) {
 
   return (    
       <div className={"slide slide-" + props.slide.id}> 
-                <div className='img-wrapper'>
-                    <img src={ASSETS+props.slide.img}/>
+                <div className='img-wrapper'
+                style={{ 
+                  backgroundImage: `url("${ASSETS+props.slide.img}")` 
+                }}
+                >
                 </div>
                 <div className='info-wrapper'>
                    <div className='info'><h2 className='info-sell'>{props.slide.info}</h2></div> 
