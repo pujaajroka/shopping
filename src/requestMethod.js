8ebca2f92f64b3ec346c0fe448550694c0d49e65
DESKTOP-72D0N0M\Dell <webfusionsolution@gmail.com>
2023-04-06 09:15:25 +0530

    xtyles client changes - heruko url deleted

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://sellitwell.herokuapp.com/api/";
+const BASE_URL = "http://api.xtyles.in/api/";
 //const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MGE4ZjA5MDJjMmIyMGUxMmYwZjE3ZSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3OTU1MjUwOCwiZXhwIjoxNjc5ODExNzA4fQ.VQRtmcFkr5VhKIFp6chIbmyZaQQAojGNAldpD31Xp38"
 export const publicRequest = axios.create({
@@ -9,5 +9,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
     baseURL:BASE_URL, 
-    header:{token: `Bearer ${TOKEN}`},
+    headers:{token: `Bearer ${TOKEN}`},
 })
